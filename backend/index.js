e0abbd20f5a039305662f22c979e8343a5322512
Yash <yashmangal240@gmail.com>
2024-06-20 15:22:55 +0530

    deploy successfully

--- backend/index.js
+++ backend/index.js
@@ -8,13 +8,25 @@ const cors = require("cors");
 const insertData = require("./config/addingData");
 const dataRoute = require("./routes/dataRoute");
 
-connectDB().then(() => {
-  insertData();
-});
+connectDB()
+  .then(() => {
+    console.log("Database connected successfully");
+    insertData()
+      .then(() => {
+        console.log("Data inserted successfully");
+      })
+      .catch((err) => {
+        console.error("Error inserting data:", err);
+      });
+  })
+  .catch((err) => {
+    console.error("Database connection error:", err);
+  });
 
 app.use(cors());
 
 app.get('/', (req, res) => {
+  console.log("Root route accessed");
   res.send('Yay!! Backend is running for Blackcoffer Assignment');
 });
 
@@ -23,9 +35,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/data", dataRoute);
 
-app.listen(process.env.PORT, () => {
-  console.log("Server is running on port 4000");
-});
+const PORT = process.env.PORT || 4000;
 
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 module.exports = app;
